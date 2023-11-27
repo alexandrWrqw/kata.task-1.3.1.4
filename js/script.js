ba4ffcc451623040fc4078c9cae87eb1fd4c22d1
Alexandr Lisovoy <sashaliss18@gmail.com>
2023-11-27 16:51:57 +0500

    swiper

--- js/script.js
+++ js/script.js
@@ -1,42 +1,61 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const width = window.innerWidth
-//     if (width < 768) {
-//     }
-// })
+// swiper
+window.addEventListener('DOMContentLoaded', () => {
+    const resizeSwiper = (breakpoint, swiperClass, swiperSettings) => {
+        let swiper;
 
-const changeDirection = (swiper) => {
-    window.innerWidth <= 768 ? swiper.destroy(true, true) : slider.init();
-};
+        breakpoint = window.matchMedia(breakpoint);
 
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
+        const enableSwiper = function(className, settings) {
+            swiper = new Swiper(className, settings);
+        }
+        
+        const checkBreakpointDestroy = function() {
+            if (breakpoint.matches) { // попадаем ли мы в промежуток медиа 
+                return enableSwiper(swiperClass, swiperSettings);
+            } else {
+                if (swiper !== undefined) { // если свайпера нет, то не нужно удалять его
+                    swiper.destroy(true, true); // удалить инстанс и стили
+                };
+                return;
+            }
+        };
 
-    pagination: {
-    el: '.swiper-pagination',
-    clicable: true,
-    },
+        breakpoint.addEventListener('change', checkBreakpointDestroy);
+        checkBreakpointDestroy();
+    };
 
-    slidersPerView: 2,
-    
-    watchOverflow: true,
+    resizeSwiper(
+        '(max-width: 580px)',
+        '.slider-brands', // отдельный класс для этого свайпера
+        {
+            direction: 'horizontal',
+            loop: false,
+            spaceBetween: 16,
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+                clickable: true,
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                360: {
+                    slidesPerView: 1.2,
+                },
+                420: {
+                    slidesPerView: 1.6,
+                },
+            },
+        }
+    );
+});
 
-    spaceBetween: 16,
+// show btn
+const showBrands = document.querySelector('.brands__show-btn');
+const listBrands = document.querySelector('.brands__list');
 
-    //     autoplay: {
-    //         delay: 2000,
-    //         stopOnLastSlide: false,
-    //         disableOnInteraction: true,
-    //     },
-
-    // speed: 800,
-
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-        },
-        480: {
-            slidesPerView: 2,
-        },
-    },
-
+showBrands.addEventListener('click', function() {
+    listBrands.classList.toggle('brands__list--visible');
+    this.textContent = this.textContent === 'Показать все' ? 'Скрыть' : 'Показать все';
 });
